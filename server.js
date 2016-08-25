76ae44f5345701a897a5e44c9da91a3d3047d4f1
Jason Robinson <jcr.compstat@gmail.com>
2016-08-25 10:31:31 -0700

    Additional updates for middleware

--- server.js
+++ server.js
@@ -4,17 +4,6 @@ var PORT = 3000;
 
 var middleware = require('./middleware.js')
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + " " + req.method, + " " + req.originalUrl);
-        next();
-    }
-}
-
 // remove this when adding public
 // app.get('/', function (req, res) {
 //     res.send('Hello Express!');
